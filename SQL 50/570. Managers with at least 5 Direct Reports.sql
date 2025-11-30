5276ac1ea4fdfe4f6c3c651d5240905a983fe38d
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2025-11-30 17:26:39 +0530

    Create 570. Managers with at least 5 Direct Reports.sql

---
+++ SQL 50/570. Managers with at least 5 Direct Reports.sql
@@ -0,0 +1,28 @@
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+
+SELECT m.name
+FROM Employee e
+JOIN Employee m ON e.managerID = m.id
+GROUP BY m.name, m.id
+HAVING COUNT(e.managerID) >= 5
+;
